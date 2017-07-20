13b0263562b802207f9635d61db095432d3b8cb6
Юра <@DESKTOP-M0EHSPI>
2017-07-20 17:03:30 +0300

    Second coment

--- Home5/src/dz/Main.java
+++ Home5/src/dz/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
+		System.out.println("wwwwww");
+		System.out.println("tttttt");
 	}
 
 }
