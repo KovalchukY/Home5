5e114c43b9fc34fcf557459bbe3d6bf05785497a
Юра <@DESKTOP-M0EHSPI>
2017-07-20 17:02:13 +0300

    First commit

---
+++ Home5/src/dz/Main.java
@@ -0,0 +1,11 @@
+package dz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+	}
+
+}
